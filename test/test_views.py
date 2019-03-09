d49815da0153df911c9850051f15ed389eac3b13
student <tester@tester-m>
2019-03-09 13:29:06 +0100

    z lintem

--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Katerina", "msg":"Hello World!"}', rv.data)
+        self.assertEquals('{ "imie":"Katerina", "msg":"Hello World!"}',
+                          rv.data)
